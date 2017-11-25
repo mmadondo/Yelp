4289adc228a7dc369a7192a0e9834af90e12f198
Malvern <mmadondo@users.noreply.github.com>
2017-11-25 13:28:31 -0600

    Added UI features

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,30 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Malvern Madondo on 11/25/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
